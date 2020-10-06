82cc62efdb9fdd09ade6b3d03cf9b2f823d72db5
CarolinaMoura <36082306+CarolinaMoura@users.noreply.github.com>
2020-10-06 10:39:16 -0300

    Update pastiri.cpp

--- COI/2020/pastiri.cpp
+++ COI/2020/pastiri.cpp
@@ -16,95 +16,66 @@ const int MAXN = 5e5+10 ;
 
 using namespace std ;
 
-int n , k  ;
-int prof[MAXN] , minToSheep[MAXN] ;
-int cobertura[MAXN] , dist[MAXN] ;
+int n , k ;
+int dist[MAXN] , lvl[MAXN] , ancestor[MAXN] ;
+vector<int> sheep ;
 vector<int> adj[MAXN] ;
 bool hasSheep[MAXN] ;
+bool prot[MAXN] ;
 
-void dfs1(int x, int father)
+void bfs()
 {
 
-    prof[x] = (hasSheep[x]) ? 0 : (n+5) ;
+    lp(i,1,n+1) dist[i] = n+5 ;
+
+    vector<int> fila ;
+    int ini = 0 ;
+
+    for(auto e : sheep ) 
+    {
+        dist[e] = 0 ;
+        fila.pb(e) ;
+    }
+
+    while(ini < sz(fila))
+    {
+        int x = fila[ini++] ;
+        for(auto e : adj[x])
+        {
+            if(dist[e] <= dist[x]+1) continue ;
+            dist[e] = dist[x]+1 ;
+            fila.pb(e) ;
+        }
+    }
+}
+
+void dfs(int x, int father, int curMax ) 
+{
+
+    if(lvl[x]+dist[x] > lvl[curMax]+dist[curMax])
+        curMax = x ;
+
+    if( hasSheep[x] )
+        ancestor[x] = curMax ;
 
     for(auto e : adj[x])
     {
         if(e == father) continue ;
-
-        dfs1(e,x) ;
-        prof[x] = min(prof[x] , prof[e]+1) ;
-
-    }
-
-}
-
-void dfs2(int x, int father )
-{
-    int qtd = sz(adj[x]) ;
-    vector<int> pref(qtd) , suf(qtd) ;
-
-    prof[x] = hasSheep[x] ? 0 : (n+5) ;
-
-    for(int i = 0 ; i < qtd ; i++ )
-        pref[i] = suf[i] = prof[ adj[x][i] ] + 1 ;
-
-    for(int i = 1 , ii = qtd-2 ; i < qtd ; i++ , ii-- )
-    {
-        pref[i] = min(pref[i-1] , pref[i]) ;
-        suf[ii] = min(suf[ii], suf[ii+1]) ;
-    }
-
-    prof[x] = minToSheep[x] = min(prof[x], suf[0] ) ;
-
-    for(int i = 0, neigh ; i < qtd ; i++ )
-    {
-        neigh = adj[x][i] ;
         
-        if(neigh == father) continue ;
-
-        int saveX = prof[x] ;
-        int saveNeigh = prof[neigh] ;
-
-        prof[x] = hasSheep[x] ? 0 : n+5 ;
-
-        if(i) prof[x] = min(pref[i-1] , prof[x] ) ;
-        if( i+1 < qtd ) prof[x] = min(prof[x] , suf[i+1] ) ;
-
-        dfs2(neigh, x) ;
-
-        prof[x] = saveX ;
-        prof[neigh] = saveNeigh ;
+        lvl[e] = lvl[x]+1 ;
+        dfs(e,x,curMax) ;
 
     }
-
 }
 
-vector<int> ans ;
-void dfs3(int x, int father)
+void dfsProtect(int x)
 {
-
-    for(auto e : adj[x] ) 
-        if(e != father) dfs3(e, x) ;
-
-    if(hasSheep[x]) dist[x] = 0 ;
-
-    if(cobertura[x] != -1 && cobertura[x] >= dist[x])
-        dist[x] = -1 ;
-
-    if( dist[x] != -1 )
+    prot[x] = true ;
+    for(auto e :adj[x])
     {
-
-        if(father == -1 || minToSheep[father] != dist[x]+1 )
-        {
-            cobertura[x] = dist[x] ;
-            ans.push_back(x) ;
-        }
-        else dist[father]=dist[x]+1;
+        if(prot[e] || dist[e] != dist[x]-1) continue ;
+        dfsProtect(e) ;
     }
-
-    if(father != -1 )
-        cobertura[father] = max(cobertura[father], cobertura[x]-1) ;
-
 }
 
 int main()
@@ -113,27 +84,34 @@ int main()
     for(int i = 0 , a , b ; i <n-1 ; i++ )
     {
         scanf("%d%d", &a, &b ) ;
-        adj[a].pb(b);
+
+        adj[a].pb(b); 
         adj[b].pb(a) ;
     }
-    for(int i = 0 ,x  ; i < k ; i++ )
+
+    sheep.resize(k) ;
+
+    for(int i = 0 ; i < k ; i++ )
     {
-        scanf("%d", &x ) ;
-        hasSheep[x] = true ;
+        scanf("%d", &sheep[i]) ;
+        hasSheep[ sheep[i] ] = true ;
     }
 
-    dfs1(1,-1) ;
-    dfs2(1,-1) ;
+    bfs() ;
+    dfs(1,-1, 1 ) ;
 
- //   debug("Debugging minToSheep:\n") ;
-   // lp(i,1,n+1) debug("%d%c", minToSheep[i], " \n"[i == n]) ;
+    sort(all(sheep), [&](int a, int b){return lvl[a] > lvl[b] ;}) ;
 
-    lp(i,1,n+1) dist[i] = cobertura[i] = -1 ;
+    vector<int> ans ;
 
-    dfs3(1,-1) ;
+    for(int s : sheep)
+    {
+        if(prot[s]) continue ;
+        ans.push_back(ancestor[s]) ;
+        dfsProtect( ancestor[s] ) ;
+    }
 
     sort(all(ans)) ;
-    printf("%d\n" , sz(ans)) ;
-    for(int i = 0 ; i < sz(ans) ; i++ ) printf("%d%c", ans[i], " \n"[i == sz(ans)-1]) ;
-
+    printf("%d\n", sz(ans) ) ;
+    for(int i = 0 ; i < sz(ans) ; i++ ) printf("%d%c", ans[i], " \n"[i+1 == sz(ans)] ) ;
 }
