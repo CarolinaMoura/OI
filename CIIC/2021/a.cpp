cdd91be5f018a007dd7cf5da355be0842b182e6a
CarolinaMoura <36082306+CarolinaMoura@users.noreply.github.com>
2021-06-02 14:25:52 -0300

    Update a.cpp

--- CIIC/2021/a.cpp
+++ CIIC/2021/a.cpp
@@ -1,55 +1,48 @@
 #include <bits/stdc++.h>
 
-//Ainda nao consegui submeter
-
 const int MAXN = 35 ;
-
+ 
 using namespace std ;
-
+ 
 int N , mask ;
 int mat[MAXN][MAXN] ;
 unordered_map<int, int> dp ;
-
-void solve()
+ 
+int solve()
 {
-	if( dp.find(mask) != dp.end() ) return ;
-
+	if( mask == 0 ) return 0 ;
+	if( dp[mask] ) return dp[mask] ;
+ 
 	int k = __builtin_ctz(mask) ;
 	int ans = 0 ;
-
+ 
 	mask ^= (1<<k) ;
-
+ 
 	for(int i = k+1 ; i < N ; i++ )
 	{
 		if( ((1<<i)&mask ) == 0 ) continue ;
-
+ 
 		mask ^= (1<<i) ;
-
-		solve() ;
-
-		ans = max(ans, mat[i][k]+dp[mask] ) ;
-
+  
+		ans = max(ans, mat[i][k]+solve() ) ;
+ 
 		mask ^= (1<<i) ;
-
+ 
 	}
-
+ 
 	mask ^= (1<<k) ;
-
-	dp.insert( make_pair(mask, ans) ) ;
-
+ 
+	return dp[mask] = ans ; 
 }
-
+ 
 int main()
 {
 	scanf("%d", &N ) ;
 	for(int i = 0 ; i < N ;i++ )
 		for(int j= 0 ; j < N ; j++ ) scanf("%d", &mat[i][j]) ;
-
+ 
 	mask = (1<<N) - 1 ;
-	dp.insert(make_pair(0,0) ) ;
-
-	solve() ;
-
-	printf("%d\n", dp[mask] ) ;
-
+  
+	printf("%d\n", solve() ) ;
+ 
 }
