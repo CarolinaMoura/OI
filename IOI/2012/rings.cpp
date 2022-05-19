14739247638c3c02b3872340e033d766ea8e3cb1
CarolinaMoura <36082306+CarolinaMoura@users.noreply.github.com>
2022-05-19 15:29:22 -0300

    Update rings.cpp

--- IOI/2012/rings.cpp
+++ IOI/2012/rings.cpp
@@ -1,180 +1,96 @@
 #include <bits/stdc++.h>
-
-#define pii pair<int,int>
-#define mk make_pair
-#define lp(i,a,b) for(int i = a ; i < b ; i++)
-#define ff first
-#define ss second
-#define pb push_back
-#define ll long long
-#define debug printf
-
+ 
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)(x.size())
+ 
 const int MAXN = 1e6+10 ;
-
+ 
 using namespace std ;
-
-
-struct Graph
-{
-
-    int ans , N ;
-    int pai[MAXN] , deg[MAXN] ;
-    vector<int> adj[MAXN] ;
-    bool marc[MAXN] , can_be[MAXN] , exist[MAXN] ;
-    bool operational ;
-
-    Graph()
-    {
-        N = MAXN - 5 ;
-        lp(i,1,N+1)
-        {
-            deg[i] = 0 ;
-            marc[i] = false ;
-            can_be[i] = true ;
-            pai[i] = i ;
-            exist[i] = true ;
-        }
-        operational = true ;
-        ans = N ;
-    }
-
-    int get_return() { return operational ; }
-
-    int find(int x)
-    {
-        if( x == pai[x] ) return x ;
-        return pai[x] = find( pai[x] ) ;
-    }
-
-    bool join(int x, int y)
-    {
-        x = find(x) ;
-        y = find(y) ;
-
-        if( x == y ) return false ;
-
-        if( rand() % 2 ) swap(x,y) ;
-
-        pai[x] = y ;
-
-        return true ;
-
-    }
-
-    bool dfs(int x, int desired )
-    {
-        marc[x] = true ;
-        bool can = ( x == desired ) ;
-
-        for(int i : adj[x] )
-            if( !marc[i] ) can |= dfs(i, desired) ;
-
-        if(can) ans += can_be[x] ;
-        else can_be[x] = false ;
-
-        return can ;
-
-    }
-
-    void make_link(int A, int B)
-    {
-
-        if( !exist[A] || !exist[B] ) return ;
-
-        if( !join(A,B) || max( ++deg[A] , ++deg[B] ) >= 3 ) operational = false ;
-
-        adj[A].pb(B) ;
-        adj[B].pb(A) ;
-
-    }
-
-
-} ;
-
-int N , L ;
-bool forest_ok ;
-Graph graph[5] ;
-vector<pii> edges ;
-
-inline void make_partition(int center)
-{
-
-    vector<int> List ;
-
-    List.pb(center) ;
-    for(int i : graph[0].adj[center] ) List.pb( i ) ;
-
-    for(int i = 0 , idx = 1 ; i < List.size() ; i++ , idx ++ )
-    {
-        graph[idx].exist[ List[i] ] = false ;
-        for(auto p : edges ) graph[idx].make_link(p.ff, p.ss) ;
-    }
-    forest_ok = true ;
-    graph[0].ans = 0 ;
-    lp(i,1,5) graph[0].ans += graph[i].operational ;
+ 
+int N ;
+vector<pair<int,int> > edg ;
+int dsu[5][MAXN] , qtd[5][MAXN] , deg[5][MAXN] , qtdCycle[5] , sz_cycle[5] ;
+int bigDegree[5] , guy[5] ;
+ 
+int find(int x, int idx){
+  return dsu[idx][x] = ( x == dsu[idx][x]) ? x : find(dsu[idx][x],idx) ;
 }
-
-int main()
-{
-    scanf("%d%d", &N , &L ) ;
-    graph[0].ans = N ;
-
-    while( L-- )
-    {
-        int A , B ;
-
-        scanf("%d", &A ) ;
-
-        if( A == -1 )
-        {
-            printf("%d\n",  graph[0].ans ) ;
-            continue ;
-        }
-
-        scanf("%d", &B ) ;
-        A++ ; B++ ;
-
-        if( graph[0].ans == 0 ) continue ;
-
-        edges.pb( mk(A,B) ) ;
-
-        if( !forest_ok )
-        {
-            graph[0].deg[A] ++ ;
-            graph[0].deg[B] ++ ;
-
-            if( graph[0].deg[A] < graph[0].deg[B] ) swap(A,B) ;
-
-            if( graph[0].deg[A] == 3 )
-            {
-                graph[0].adj[A].pb(B) ;
-                graph[0].adj[B].pb(A) ;
-
-                make_partition(A) ;
-                continue ;
-            }
-
-            if( !graph[0].join(A,B) )
-            {
-                graph[0].ans = 0 ;
-                lp(i,1,N+1) graph[0].marc[i] = false ;
-                graph[0].dfs(A, B) ;
-                lp(i,1,N+1)
-                    if( !graph[0].marc[i] )
-                        graph[0].dfs(i,A) ;
-            }
-
-            graph[0].adj[A].pb(B) ;
-            graph[0].adj[B].pb(A) ;
-            continue;
-        }
-
-        for(int i = 1 ; i <= 4 ; i++ )
-        {
-            graph[0].ans -= graph[i].operational ;
-            graph[i].make_link(A,B) ;
-            graph[0].ans += graph[i].operational ;
-        }
-
+ 
+int isCycle(int x, int y, int idx ){
+  x = find(x,idx) ;
+  y = find(y,idx) ;
+ 
+  if(x == y  )return true ;
+ 
+  if(rand() % 2) swap(x,y) ;
+ 
+  dsu[idx][x] = y ;
+  qtd[idx][y] += qtd[idx][x] ;
+ 
+  return false ;
+}
+ 
+void addEdge(int A, int B, int idx){
+ 
+  if(A == guy[idx] || B == guy[idx]) return ;
+ 
+  bigDegree[idx] = max(bigDegree[idx],++deg[idx][A]) ;
+  bigDegree[idx] = max(bigDegree[idx],++deg[idx][B]) ;
+ 
+  if(isCycle(A,B,idx)){
+    qtdCycle[idx]++ ;
+    sz_cycle[idx] = qtd[idx][find(A,idx)] ;
+  }
+ 
+}
+ 
+void Init(int N_) {
+  N = N_;
+  guy[0] = -1 ;
+  sz_cycle[0] = N ;
+  for(int i= 0 ; i < 5 ; i++ )
+    for(int j = 0 ; j < N ; j++ ){
+      qtd[i][j] = 1 ;
+      dsu[i][j] = j ;
     }
+}
+ 
+void Link(int A, int B) {
+  
+  edg.push_back(make_pair(A,B)) ;
+  bool spoiled = (bigDegree[0] >= 3) ;
+ 
+  addEdge(A,B,0) ;
+  if(spoiled)
+    for(int i = 1 ; i < 5 ; i++ ) addEdge(A,B,i) ;
+ 
+  if(deg[0][A] < deg[0][B]) swap(A,B) ;
+  if(bigDegree[0] < 3 || spoiled ) return ;
+ 
+  guy[1] = A ;
+  int id = 1 ;
+  for(auto &e : edg ){
+    if(e.first == A ) swap(e.first, e.second );
+    if(e.second == A) guy[++id] = e.first ;
+  }
+ 
+  for(auto &e : edg )
+    for(int i = 1 ;i  < 5 ; i++ )
+      addEdge(e.first, e.second , i ) ;
+}
+ 
+int CountCritical() {
+ 
+  int ans = 0 ;
+  if(bigDegree[0] < 3 && qtdCycle[0] <= 1){
+    ans += sz_cycle[0] ;
+  }
+ 
+  if(bigDegree[0] >= 3 ){
+    for(int i = 1 ; i < 5 ; i++ ) 
+      ans += (bigDegree[i] < 3 && qtdCycle[i] == 0 ) ;
+  }
+ 
+  return ans ;
+ 
 }
